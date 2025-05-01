2e374a5541008cebffe8f21900287c4a7b0bfdd9
Enix Yu <enix223@163.com>
2025-05-01 15:45:20 +0000

    feat: add sp pointer init

--- app/ledc/start.s
+++ app/ledc/start.s
@@ -4,3 +4,6 @@ __start:
     bic r0, r0, #0x1f ;clear cpsr bit4~0
     orr r0, r0, #0x13 ; enable svc mode
     msr cpsr, r0 ; write r0 to cpsr
+
+    ldr sp, =0x80200000 ; set sp pointer to 0x8020000 (2MB stack size)
+    b main
