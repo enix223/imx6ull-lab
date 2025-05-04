405dc0b72a51d3090d2a120b329d949b3348d543
Enix Yu <enix223@163.com>
2025-05-04 14:46:30 +0000

    fix: prevent install lvgl library; make lvgl static linked

--- app/buildroot/package/lvgl-app/lvgl-app.mk
+++ app/buildroot/package/lvgl-app/lvgl-app.mk
@@ -9,4 +9,13 @@ LVGL_APP_LICENSE_FILES = LICENSE
 LVGL_APP_SUBDIR = .
 LVGL_APP_CONF_OPTS += -DCMAKE_INSTALL_PREFIX=/usr
 
+# Prevent installing anything from CMake
+define LVGL_APP_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/lvgl-app $(TARGET_DIR)/usr/bin/lvgl-app
+endef
+
+define LVGL_APP_INSTALL_STAGING_CMDS
+    # Empty if no need for staging
+endef
+
 $(eval $(cmake-package))
